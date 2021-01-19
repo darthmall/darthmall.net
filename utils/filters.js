96c457d4e0b4814f159d42971b7a6e93ff3ca687
W. Evan Sheehan <52247+darthmall@users.noreply.github.com>
2021-01-18 21:46:04 -0700

    Hide likes in webmentions
    For now, at least, I'm opting to hide likes. I'm mainly interested in people
    that have an actual response to something I wrote. Displaying likes feels too
    much like Twitter.

--- utils/filters.js
+++ utils/filters.js
@@ -44,7 +44,9 @@ function isoDateFilter(value) {
 }
 
 function webmentionsForUrl(webmentions, url) {
-  return webmentions.children.filter((mention) => mention['wm-target'] === url);
+  return webmentions.children.filter((mention) => {
+    return mention['wm-target'] === url && mention['wm-property'] !== 'like-of';
+  });
 }
 
 module.exports = {

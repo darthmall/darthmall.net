bc36bcffe89d737c443fee4222c018877bc5225e
W. Evan Sheehan <darth_mall@pm.me>
2022-02-02 07:59:10 -0700

    Implement theme switching
    I had to make some adjustments to the CSS to avoid specificity and
    cascade problems setting the theme variables, but the toggle seems to be
    working now.

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,5 +3,9 @@ window.toggleTheme = function (event) {
     || !event.target.hasAttribute('data-toggle-theme')) return;
 
   const theme = event.target.dataset.toggleTheme;
-  console.log(`Toggle ${theme}`);
+  if (theme === 'system') {
+    document.documentElement.removeAttribute('data-theme');
+  } else {
+    document.documentElement.dataset.theme = theme;
+  }
 };

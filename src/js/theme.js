5ebd5a6273e0ca54ea2fe9d4219d57676ca08224
W. Evan Sheehan <darth_mall@pm.me>
2022-02-02 14:35:09 -0700

    Manage UI state for the toggle
    Update the aria-pressed attribute on each button based on the current
    theme. This seems to be how Adrian Roselli does it, and I trust him to
    make appropriate use of ARIA.

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,9 +1,12 @@
 (function () {
+  const TOGGLE_CLASS = '.theme-toggle';
+  const THEME_KEY = 'theme-preference';
+
   function init() {
     // Remove the theme toggle if localStorage is not supported, because we
     // can't store the visitor's preferences.
     if (!window.localStorage) {
-      const toggles = document.querySelector('.theme-toggle');
+      const toggles = document.querySelector(TOGGLE_CLASS);
       for (let el of toggles) {
         el.remove();
       }
@@ -12,17 +15,20 @@
 
     const theme = localStorage.getItem(THEME_KEY);
     if (theme) {
-      document.documentElement.setAttribute('data-theme', theme);
+      toggleTheme(theme)
     }
+
+    window.onThemeToggle = onThemeToggle;
   }
 
-  const THEME_KEY = 'theme-preference';
-
-  window.toggleTheme = function (event) {
+  function onThemeToggle(event) {
     if (event.target.tagName !== 'BUTTON'
       || !event.target.hasAttribute('data-toggle-theme')) return;
 
-    const theme = event.target.dataset.toggleTheme;
+    toggleTheme(event.target.getAttribute('data-toggle-theme'));
+  }
+
+  function toggleTheme(theme) {
     if (theme === 'system') {
       document.documentElement.removeAttribute('data-theme');
       localStorage.removeItem(THEME_KEY);
@@ -30,7 +36,18 @@
       document.documentElement.dataset.theme = theme;
       localStorage.setItem(THEME_KEY, theme);
     }
-  };
 
-  init();
+    // Update UI
+    document.querySelectorAll(
+      `${TOGGLE_CLASS} [data-toggle-theme]`
+    ).forEach(function (el) {
+      if (el.getAttribute('data-toggle-theme') === theme) {
+        el.setAttribute('aria-pressed', 'true');
+      } else {
+        el.removeAttribute('aria-pressed');
+      }
+    });
+  }
+
+  window.addEventListener('DOMContentLoaded', init);
 })()

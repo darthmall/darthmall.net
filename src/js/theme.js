95277d5a6a50f96a21f65e4ab100a8c608cb9d09
W. Evan Sheehan <darth_mall@pm.me>
2022-02-02 08:30:02 -0700

    Write theme preference to local storage

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,11 +1,17 @@
-window.toggleTheme = function (event) {
-  if (event.target.tagName !== 'BUTTON'
-    || !event.target.hasAttribute('data-toggle-theme')) return;
+(function () {
+  const THEME_KEY = 'theme-preference';
 
-  const theme = event.target.dataset.toggleTheme;
-  if (theme === 'system') {
-    document.documentElement.removeAttribute('data-theme');
-  } else {
-    document.documentElement.dataset.theme = theme;
-  }
-};
+  window.toggleTheme = function (event) {
+    if (event.target.tagName !== 'BUTTON'
+      || !event.target.hasAttribute('data-toggle-theme')) return;
+
+    const theme = event.target.dataset.toggleTheme;
+    if (theme === 'system') {
+      document.documentElement.removeAttribute('data-theme');
+      localStorage.removeItem(THEME_KEY);
+    } else {
+      document.documentElement.dataset.theme = theme;
+      localStorage.setItem(THEME_KEY, theme);
+    }
+  };
+})()

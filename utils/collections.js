aef4435ac21f2c3610adb6a03dfbde1a17c89cea
W. Evan Sheehan <darth_mall@pm.me>
2022-02-04 14:26:20 -0700

    Rename work collection to portfolio
    Might as well keep naming consistent while I'm thinking of it.

--- utils/collections.js
+++ utils/collections.js
@@ -1,5 +1,31 @@
 const site = require('../src/_data/site.json');
 
+function portfolio(collection) {
+  return collection
+    .getFilteredByGlob('./src/portfolio/**/*.md')
+    .filter(post => !post.data.draft)
+    .sort((a, b) => {
+      // Sort by the order field first, placing older projects after newer projects
+      if (a.data.order > b.data.order) return -1;
+      if (a.data.order < b.data.order) return 1;
+
+      // If the order fields are identical fallback to sorting by title
+      if (a.data.title < b.data.title) return -1;
+      if (a.data.title > b.data.title) return 1;
+
+      return 0;
+    })
+    .map((p, i, posts) => {
+      const prev = posts[i - 1],
+            next = posts[i + 1];
+
+      if (prev) p.data.prev = prev.data;
+      if (next) p.data.next = next.data;
+
+      return p;
+    });
+}
+
 function posts(collection) {
   const now = new Date(),
     livePosts = (post) => post.date <= now && !post.data.draft;
@@ -29,35 +55,9 @@ function recent(collection) {
     .slice(0, site.recentPostCount);
 }
 
-function work(collection) {
-  return collection
-    .getFilteredByGlob('./src/portfolio/**/*.md')
-    .filter(post => !post.data.draft)
-    .sort((a, b) => {
-      // Sort by the order field first, placing older projects after newer projects
-      if (a.data.order > b.data.order) return -1;
-      if (a.data.order < b.data.order) return 1;
-
-      // If the order fields are identical fallback to sorting by title
-      if (a.data.title < b.data.title) return -1;
-      if (a.data.title > b.data.title) return 1;
-
-      return 0;
-    })
-    .map((p, i, posts) => {
-      const prev = posts[i - 1],
-            next = posts[i + 1];
-
-      if (prev) p.data.prev = prev.data;
-      if (next) p.data.next = next.data;
-
-      return p;
-    });
-}
-
 module.exports = {
+  portfolio,
   posts,
   recent,
   sketches,
-  work,
 };

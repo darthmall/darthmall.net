f920e030adb361c05c013172aad7ea16236aae17
W. Evan Sheehan <darth_mall@pm.me>
2022-08-07 16:49:19 -0600

    Subset Sporting Grotesque and Epilogue
    I added a script that uses pyftsubset and glyphhanger to subset my main fonts,
    removing opentype features I don't want and eliminating characters I shouldn't
    need. Epilogue is about 1/3 as big as it was. Savings on Sporting Grotesque are
    less significant, but it's still good to eliminate some dead weight.
    
    I might be able to trim the character set for Sporting Grotesque even further
    because I mainly use it for titles.

---
+++ scripts/subset.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+RANGE="$(glyphhanger --LATIN)"
+US="$(glyphhanger --US_ASCII)"
+
+pyftsubset \
+  'src/fonts/Epilogue[slnt,wght].woff2' \
+  --unicodes="${RANGE}" \
+  --flavor=woff2 \
+  --layout-features="ccmp,locl,mark,mkmk,kern,liga,frac,pnum,zero,ss01,smcp,subs,sups,cpsp,case" \
+  --output-file='src/fonts/Epilogue[slnt,wght]-latin.woff2'
+
+pyftsubset \
+  'src/fonts/SportingGrotesque-Bold.woff2' \
+  --unicodes="${RANGE}" \
+  --flavor=woff2 \
+  --layout-features+="titl,onum" \
+  --layout-features-="ss01,lnum" \
+  --output-file='src/fonts/SportingGrotesque-Bold-latin.woff2'
+
+pyftsubset \
+  'src/fonts/SportingGrotesque.woff2' \
+  --unicodes="${RANGE}" \
+  --flavor=woff2 \
+  --layout-features+="titl,onum" \
+  --layout-features-="ss01,lnum" \
+  --output-file='src/fonts/SportingGrotesque-latin.woff2'
